8ebbfa784d1f921c4b6872c5ea0c6a8a001a6941
Pedro Berrío Valdor <pedro.berrio@lisdatasolutions.com>
2024-11-28 11:40:22 +0100

    Auth modificado

--- routers/v1/controllers/authController.py
+++ routers/v1/controllers/authController.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from fastapi import FastAPI, APIRouter, Depends, HTTPException
 from pydantic import BaseModel
-from models.user import Users
 from passlib.context import CryptContext
 from typing import Annotated, Dict
 from starlette import status
@@ -11,7 +10,7 @@ from jose import JWTError, jwt
 from config.db_depend import get_db
 from config.user_depend import get_current_user, authenticate_user, create_access_token
 from schemas.user import User
-from config.user_depend import bcrypt_context
+
 from schemas.token import Token
 from passlib.context import CryptContext
 from schemas.user import User
@@ -29,21 +28,6 @@ class AuthController:
     def __init__(self):
         self.service = UserService()
 
-
-    #Metodo para crear un nuevo usuario
-    @router.post("/", status_code=status.HTTP_201_CREATED)
-    async def create_user(self,create_user_request: User,db:Session = Depends(get_db)):
-        create_user_model = Users(
-            email=create_user_request.email,
-            username=create_user_request.username,
-            first_name=create_user_request.first_name,
-            last_name=create_user_request.last_name,
-            role=create_user_request.role,
-            hashed_password=bcrypt_context.hash(create_user_request.password),
-            is_active=True
-        )
-
-        self.service.createUser(create_user_model, db)
 
     #Metodo que devuelve el token siempre que el usuario este autenticado
     @router.post("/token", response_model=Token)
